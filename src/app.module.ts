425388ab29a2846720bc7303b40fe0adde631e2c
Muhammad Umer Saeed <umersaeed3d@gmail.com>
2022-09-24 14:33:22 +0500

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,52 @@
+import { Module, RequestMethod, MiddlewareConsumer } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { JwtModule } from '@nestjs/jwt';
+import { secret } from './utils/constants';
+import { join } from 'path/posix';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+import { VideoController } from './controller/video.controller';
+import { VideoService } from './service/video.service';
+import { UserService } from './service/user.service';
+import { UserController } from './controller/user.controller';
+import { Video, VideoSchema } from './model/video.schema';
+import { User, UserSchema } from './model/user.schema';
+import { isAuthenticated } from './app.middleware';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/Stream'),
+    JwtModule.register({
+      secret,
+      signOptions: { expiresIn: '2h' },
+    }),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([{ name: Video.name, schema: VideoSchema }]),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    MulterModule.register({
+      storage: diskStorage({
+        destination: './public',
+        filename: (req, file, cb) => {
+          const ext = file.mimetype.split('/')[1];
+          cb(null, `${uuidv4()}-${Date.now()}.${ext}`);
+        },
+      }),
+    }),
+  ],
+  controllers: [AppController, VideoController, UserController],
+  providers: [AppService, VideoService, UserService],
+})
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(isAuthenticated)
+      .exclude({ path: 'api/v1/video/:id', method: RequestMethod.GET })
+      .forRoutes(VideoController);
+  }
+}
